cbe8a1ffd203edfb29fa56337e06a9587ebf3b06
jeking123 <azunaki1@gmail.com>
2016-11-08 14:32:56 -0800

    Version .5

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-// YOUR SCRIPTS
+// Grid View
+$('#grid').click(function(){
+    // Remove ONLY 'list' class
+    $('.cd-gallery').removeClass('list');
+    // ADD ONLY 'grid' Class
+    $('.cd-gallery').addClass('grid');
+});
+
+// List View
+$('#list').click(function(){
+    // Remove ONLY 'list' class
+    $('.cd-gallery').removeClass('grid');
+    // ADD ONLY 'grid' Class
+    $('.cd-gallery').addClass('list');
+});
+
+/* */
